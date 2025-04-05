c313e44a8cc2a36a917ca8bcad6415301d8849dd
noahd <noah.d.lindsey@gmail.com>
2025-04-05 18:29:10 -0400

    Firefox config addons non-free (10)

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = specialArgs;
+            # home-manager.extraSpecialArgs = specialArgs;
             home-manager.users.noah = import ./home/noah.nix;
           }
         ];
