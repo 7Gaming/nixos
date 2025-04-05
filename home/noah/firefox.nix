b07dc841761694c6018436ffb2744d3ad7c6b03c
noahd <noah.d.lindsey@gmail.com>
2025-04-05 19:13:05 -0400

    Firefox config addons non-free (16)

--- home/noah/firefox.nix
+++ home/noah/firefox.nix
@@ -4,18 +4,16 @@
   programs = {
     firefox = {
       enable = true;
-      profiles = {
-        noah = {
-          id = 0;
-          isDefault = true;
-          name = "Noah";
-          # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          #   enhancer-for-youtube # non-free
-          # ];
-          # search = {
-          #   force = true;
-          # };
-        };
+      profiles."noah" = {
+        id = 0;
+        isDefault = true;
+        name = "Noah";
+        # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        #   enhancer-for-youtube # non-free
+        # ];
+        # search = {
+        #   force = true;
+        # };
       };
     };
   };
