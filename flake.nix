9bf8fb82e2d53198ed671df0a600d1ed1be704d3
noahd <noah.d.lindsey@gmail.com>
2025-04-05 18:10:38 -0400

    Firefox config addons non-free (6)

--- flake.nix
+++ flake.nix
@@ -27,14 +27,13 @@
       nixosConfigurations.nixos = lib.nixosSystem {
         inherit system;
 
-        specialArgs = { inherit nur; };
-
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit nur; };
             home-manager.users.noah = import ./home/noah.nix;
           }
         ];
