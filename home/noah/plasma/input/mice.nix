c74c5e1e5fbe3f1dc17b2bb9f744b798ef48cd6c
noahd <noah.d.lindsey@gmail.com>
2025-04-06 15:49:32 -0400

    plasma-manager mouse

---
+++ home/noah/plasma/input/mice.nix
@@ -0,0 +1,16 @@
+inputs: {
+  programs.plasma.input.mice = [
+    {
+      enable = true;
+      name = "Default Mouse";
+
+      scrollSpeed = 1;
+      pointerSpeed = -0.25;
+
+      accelerationProfile = "none";
+      middleButtonEmulation = false;
+      naturalScroll = false;
+      leftHanded = false;
+    }
+  ];
+}
