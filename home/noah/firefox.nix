e117fd721b62ed2cd19582f2dff68ca20aaf824e
noahd <noah.d.lindsey@gmail.com>
2025-04-06 14:54:59 -0400

    Firefox settings

--- home/noah/firefox.nix
+++ home/noah/firefox.nix
@@ -4,16 +4,6 @@
   programs.firefox = {
     enable = true;
 
-    # policies = {
-    #   DisableFirefoxStudies = true;
-    #   DisablePocket = true;
-    #   DisableSafeMode = true;
-    #   DisableSetDesktopBackground = true;
-    #   DisableTelemetry = true;
-    #   DontCheckDefaultBrowser = true;
-    #   HardwareAcceleration = true;
-    # };
-
     profiles = {
       default = {
         name = "Noah";
@@ -29,13 +19,32 @@
           ];
         };
 
+        # about:config
         settings = {
           "extensions.autoDisableScopes" = 0;
-        };
 
-        # search = {
-        #   force = true;
-        # };
+          "extensions.pocket.enabled" = false;
+          "browser.shell.checkDefaultBrowser" = false;
+          "layers.acceleration.disabled" = false;
+
+          # Disable spying
+          "app.shield.optoutstudies.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "services.sync.prefs.sync.app.shield.optoutstudies.enabled" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+        };
       };
     };
   };
