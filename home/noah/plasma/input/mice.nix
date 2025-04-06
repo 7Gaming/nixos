67f696ef2071ff1a13be46fd86083f6a8de49fa4
noahd <noah.d.lindsey@gmail.com>
2025-04-06 16:19:59 -0400

    plasma-manager mouse specific

--- home/noah/plasma/input/mice.nix
+++ home/noah/plasma/input/mice.nix
@@ -2,8 +2,10 @@ inputs: {
   programs.plasma.input.mice = [
     {
       enable = true;
+      # Mouse specific data found at /proc/bus/input/devices
       name = "Logitech G203 Prodigy Gaming Mouse";
       vendorId = "046d";
+      productId = "c084";
 
       acceleration = -0.25;
 
