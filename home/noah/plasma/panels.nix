c693b1bf21f2e4f827f988fc20cf3f01a07f6c32
noahd <noah.d.lindsey@gmail.com>
2025-04-06 20:24:28 -0400

    panels

--- home/noah/plasma/panels.nix
+++ home/noah/plasma/panels.nix
@@ -1,16 +1,43 @@
 inputs: {
   programs.plasma.panels = [
     {
+      height = 32;
+      location = "top";
+      alignment = "center";
+      floating = false;
+      hiding = "normalpanel";
+      lengthMode = "fill";
+      opacity = "opaque";
+
+      widgets = [
+        # "org.kde.plasma.kickoff"
+        # "org.kde.plasma.pager"
+        # "org.kde.plasma.icontasks"
+        "org.kde.plasma.marginsseparator"
+        "org.kde.plasma.digitalclock"
+        "org.kde.plasma.marginsseparator"
+        "org.kde.plasma.systemtray"
+        # "org.kde.plasma.showdesktop"
+      ];
+    }
+
+    {
       height = 44;
+      location = "bottom";
+      alignment = "center";
+      floating = true;
+      hiding = "autohide";
+      lengthMode = "fit";
+      opacity = "opaque";
 
       widgets = [
         # "org.kde.plasma.kickoff"
         # "org.kde.plasma.pager"
         "org.kde.plasma.icontasks"
-        "org.kde.plasma.marginsseparator"
-        "org.kde.plasma.systemtray"
-        "org.kde.plasma.digitalclock"
-        "org.kde.plasma.showdesktop"
+        # "org.kde.plasma.marginsseparator"
+        # "org.kde.plasma.systemtray"
+        # "org.kde.plasma.digitalclock"
+        # "org.kde.plasma.showdesktop"
       ];
     }
   ];
